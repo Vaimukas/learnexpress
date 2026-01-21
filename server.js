6c9ec9e7c929936954b480ddf5227f558c868a96
Greg Ojasalu <greg.suur@gmail.com>
2026-01-21 11:34:36 +0200

    bootstrap and stuff

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 const app = express()
 const port = 3000
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index.njk');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk');
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://127.0.0.1:3000 ${port}`)
