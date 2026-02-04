9bd3e94eb1999fc4401d6b47a8a7de035ff59b04
Greg Ojasalu <greg.suur@gmail.com>
2026-02-04 11:33:34 +0200

    add cats

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import nunjucks from 'nunjucks';
-const app = express()
-const port = 3000
+import fs from 'fs';
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded());
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -12,12 +15,12 @@ app.get('/', (req, res) => {
   let name = 'John';
   let age = 33;
   let fruits = [
-    'Õun',
-    'Sidrun',
-    'Pirn',
+    'Banana',
     'Mango',
-    'Banaan',
-  ]
+    'Apple',
+    'Pear',
+    'Cherry',
+  ];
   res.render('index.njk', {name, age, fruits});
 });
 
@@ -33,6 +36,69 @@ app.get('/form', (req, res) => {
   res.render('form.njk');
 });
 
+app.get('/answers', (req, res) => {
+  console.log(req.body);
+  let answers = req.query;
+  answers.cool = answers.cool === 'on' ? true : false;
+  answers.age = parseInt(answers.age);
+  answers.like = parseInt(answers.like);
+  res.render('answers.njk', answers);
+});
+
+
+app.post('/answers', (req, res) => {
+  console.log(req.body);
+  let answers = req.body;
+  answers.cool = answers.cool === 'on' ? true : false;
+  answers.age = parseInt(answers.age);
+  answers.like = parseInt(answers.like);
+  res.render('answers.njk', answers);
+});
+
+
+
+
+
+app.get('/cats', (req, res) => {
+  let data = fs.readFileSync('cats.json', {encoding: 'utf-8'});
+  let cats = JSON.parse(data);
+  res.render('cats/index.njk', {cats});
+});
+
+app.get('/cats/create', (req, res) => {
+  res.render('cats/create.njk');
+});
+
+app.post('/cats', (req, res) => {
+  let data = fs.readFileSync('cats.json', {encoding: 'utf-8'});
+  let cats = JSON.parse(data);
+  let lastId = cats[cats.length-1].id;
+  let newCat = req.body;
+  newCat.id = lastId + 1;
+  newCat.spayed = newCat.spayed === "on" ? true : false;
+  newCat.birthyear = parseInt(newCat.birthyear);
+  cats.push (newCat);
+  fs.writeFileSync('cats.json', JSON.stringify(cats, null, 2));
+  res.render('cats/index.njk', {cats});
+  res.redirect('/cats');
+});
+
+app.get('/cats/view', (req, res) => {
+  let data = fs.readFileSync('cats.json', {encoding: 'utf-8'});
+  let cats = JSON.parse(data);
+  let cat = cats.find(cat => cat.id == req.query.id);
+  res.render('cats/view.njk', {cat});
+});
+
+app.get('/cats/edit', (req, res) => {
+  let data = fs.readFileSync('cats.json', {encoding: 'utf-8'});
+  let cats = JSON.parse(data);
+  let cat = cats.find(cat => cat.id == req.query.id);
+  res.render('cats/edit.njk', {cat});
+});
+
+
+
 app.listen(port, () => {
-  console.log(`Server running on http://127.0.0.1:3000 ${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
